b963747e7dc14cdb8f6fe7ed80392c501a6737bf
DM8AT <DM8AT@users.noreply.github.com>
2025-10-14 10:59:00 +0200

    added gitignore and started work on library

---
+++ src/GLGEGraphic/GLGEGraphic.h
@@ -0,0 +1,18 @@
+/**
+ * @file GLGEGraphic.h
+ * @author DM8AT
+ * @brief the main include file for the GLGE graphic core
+ * @version 0.1
+ * @date 2025-10-14
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+//header guard
+#ifndef _GLGE_GRAPHIC_
+#define _GLGE_GRAPHIC_
+
+
+
+#endif
