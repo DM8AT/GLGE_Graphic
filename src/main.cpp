978cc4d3662e3e6abbeaa027780a9a89ae11f191
DM8AT <DM8AT@users.noreply.github.com>
2025-11-02 11:54:14 +0100

    removed old debug code from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,51 +3,6 @@
 
 #include <fstream>
 
-class AssetFile : public BaseAsset
-{
-public:
-
-    AssetFile(const std::string_view& _path)
-     : path(_path)
-    {}
-
-    virtual void load() noexcept override {
-        updateLoadState(ASSET_STATE_LOADING);
-
-        //open the requested file
-        std::ifstream f(path.data(), std::ifstream::ate);
-        //sanity check the load
-        if (!f.is_open()) {
-            updateLoadState(ASSET_STATE_FAILED);
-            return;
-        }
-
-        //load the file's contents
-        size_t len = f.tellg();
-        f.seekg(std::ifstream::beg);
-        contents.resize(len);
-        f.read(contents.data(), len);
-
-        //loaded
-        updateLoadState(ASSET_STATE_LOADED);
-    }
-
-    /**
-     * @brief Get the Contents of the file
-     * 
-     * @return const std::string& a constant reference to the 
-     */
-    inline const std::string& getContents() const noexcept {return contents;}
-
-protected:
-
-    //store the path to the file
-    std::string_view path;
-    //store the contents of the file
-    std::string contents;
-
-};
-
 int main()
 {
     String p = MeshAsset::import("assets/mesh/Suzane.fbx");
